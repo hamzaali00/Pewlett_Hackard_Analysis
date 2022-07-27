71000e9a8bcc486b345dd6ecd4f3daa0b0c6d56a
hamzaali00 <100699289+hamzaali00@users.noreply.github.com>
2022-07-27 13:37:26 -0400

    Adding Deliverable 1

---
+++ Queries/Employee_Database_challenge.sql
@@ -0,0 +1,36 @@
+-- Joining employees table and titles table
+SELECT t.emp_no,
+	e.first_name,
+	e.last_name,
+	t.title,
+	t.from_date,
+	t.to_date
+INTO retirement_titles
+FROM employees AS e
+JOIN titles AS t
+ON e.emp_no = t.emp_no
+WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+ORDER BY e.emp_no;
+
+SELECT * FROM retirement_titles;
+
+-- Use Dictinct with Orderby to remove duplicate rows
+SELECT DISTINCT ON (emp_no) emp_no,
+first_name,
+last_name,
+title
+
+INTO unique_titles
+FROM retirement_titles
+WHERE to_date = ('9999-01-01')
+ORDER BY emp_no, to_date DESC;
+
+-- Retrieve number of employees by most recent job title
+SELECT COUNT(title),
+	title	
+INTO retiring_titles
+FROM unique_titles
+GROUP BY title
+ORDER BY COUNT(title) DESC;
+
+SELECT * FROM retiring_titles;
